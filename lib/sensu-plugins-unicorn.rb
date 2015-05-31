ae4a4a47ee58cfa82e819b4c368d581a167d046f
Matt Jones <mattjones@yieldbot.com>
2015-05-30 21:40:12 -0400

    update repo

--- lib/sensu-plugins-unicorn.rb
+++ lib/sensu-plugins-unicorn.rb
@@ -1,15 +1,2 @@
 
 require 'sensu-plugins-unicorn/version'
-
-# Load the defaults
-
-#
-# Default class
-#
-module SensuPluginsUnicorn
-  class << self
-  end
-
-  class << self
-  end
-end
--- lib/sensu-plugins-unicorn/version.rb
+++ lib/sensu-plugins-unicorn/version.rb
@@ -9,20 +9,5 @@ module SensuPluginsUnicorn
     PATCH = 1
 
     VER_STRING = [MAJOR, MINOR, PATCH, 'alpha.1'].compact.join('.')
-
-    NAME   = 'sensu-plugins-rabbitmq'
-    BANNER = "#{NAME} v%s"
-
-    module_function
-
-    def version
-      format(BANNER, VER_STRING)
-    end
-
-    def json_version
-      {
-        'version' => VER_STRING
-      }.to_json
-    end
   end
 end
